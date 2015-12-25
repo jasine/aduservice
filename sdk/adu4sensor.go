83613562d211ea9573bea0cf072f45d61300bbec
Zizhen Yan <zizhenyan@deepglint.com>
2015-12-25 13:20:22 +0800

    bumble http if change

--- sdk/adu4sensor.go
+++ sdk/adu4sensor.go
@@ -1,10 +1,20 @@
 package sdk
 
 import (
+	"encoding/base64"
 	"github.com/deepglint/aduservice/basic"
+	"github.com/go-martini/martini"
+	"net/http"
+	"strings"
 )
 
 var filePath string = "config/auth"
+
+// User is the authenticated username that was extracted from the request.
+type User string
+
+// BasicRealm is used when setting the WWW-Authenticate response header.
+var BasicRealm = "Authorization Required"
 
 var basicAuth *basic.BasicAdu = nil
 
@@ -22,4 +32,37 @@ func ChangePwd(username, oldpwd, newpwd string) bool {
 
 func ResetUserAndPwd() bool {
 	return basicAuth.ResetUserAndPwd()
+}
+
+// BasicFunc returns a Handler that authenticates via Basic Auth using the provided function.
+// The function should return true for a valid username/password combination.
+func BasicFunc(authfn func(string, string) bool, router []string) martini.Handler {
+	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
+		for i := 0; i < len(router); i++ {
+			if req.URL.Path == router[i] {
+				return
+			}
+		}
+		auth := req.Header.Get("Authorization")
+		if len(auth) < 6 || auth[:6] != "Basic " {
+			unauthorized(res)
+			return
+		}
+		b, err := base64.StdEncoding.DecodeString(auth[6:])
+		if err != nil {
+			unauthorized(res)
+			return
+		}
+		tokens := strings.SplitN(string(b), ":", 2)
+		if len(tokens) != 2 || !authfn(tokens[0], tokens[1]) {
+			unauthorized(res)
+			return
+		}
+		c.Map(User(tokens[0]))
+	}
+}
+
+func unauthorized(res http.ResponseWriter) {
+	res.Header().Set("WWW-Authenticate", "Basic realm=\""+BasicRealm+"\"")
+	http.Error(res, "Not Authorized", http.StatusUnauthorized)
 }
